300f61a818f32c8078e54a45990979663d6023da
Ubuntu <ubuntu@ip-172-31-14-2.ap-south-1.compute.internal>
2020-08-08 15:52:34 +0000

    added service 2

--- service2/src/app.py
+++ service2/src/app.py
@@ -1,15 +1,25 @@
 from flask import Flask
 import json
-
+import requests
+import os
 app = Flask(__name__)
-
+C1_URL= os.getenv('BASE_URL','http://172.17.0.2:6000')
 @app.route('/add')
 def add():
-    data= requests.get("http://c1:6000/calculate)
+    try:
+    	data= requests.get(C1_URL+'/calculate')
+    except Exception as e:
+        print(e)
+    print(data.json())
+    return data.json()
+
+@app.route('/')
+def index():
+    data={"data":200}
     return json.dumps(data)
 
 if __name__=="__main__":
-	app.run(host="0.0.0.0",port=6000)
+    app.run(host="0.0.0.0")
 
 
 
